9ff27b1fbb1a43730834034fc5f74f77eebbc8e2
quincerly <dan@titch.org>
2024-04-11 17:52:57 +0100

    Initial import

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Shell Prompt',
+    version='1.1',
+    description='Fancy shell prompt',
+    author='Dan Rolfe',
+      author_email='git@rolfe.email',
+    scripts=[
+        'shell_prompt',
+    ],
+)
